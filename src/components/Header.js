34cf9a9e1e9e8ac7bad8adf62c9d829e34a5733b
unknown <dafuq.ahd@gmail.com>
2019-09-18 15:50:30 +0300

    detect view port dimensions test code

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,58 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import headerVideo from "../imgs/headervideo2.mp4";
 
 const Header = () => {
+  const [dimensions, setDimensions] = useState({
+    height: window.innerHeight,
+    width: window.innerWidth
+  });
+  useEffect(() => {
+    const handleResize = () => {
+      setDimensions({
+        height: window.innerHeight,
+        width: window.innerWidth
+      });
+    };
+    window.addEventListener("resize", handleResize);
+    return _ => {
+      window.removeEventListener("resize", handleResize);
+    };
+  });
+
   return (
-    <header style={{ width: "100%", margin: 0 }}>
+    <header
+      style={{
+        width: "100%",
+        margin: 0,
+        height: "100vh",
+        marginBottom: "2rem"
+      }}
+    >
       <video
         autoPlay
         loop
         muted
-        style={{ width: "100%", height: "100vh", margin: 0, objectFit: "fill" }}
+        style={{
+          width: "100%",
+          height: "100vh",
+          margin: 0,
+          objectFit: "fill",
+          position: "absolute",
+          zIndex: -1
+        }}
       >
-        <source
-          src="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"
-          type="video/mp4"
-        ></source>
+        <source src={headerVideo} type="video/mp4"></source>
       </video>
+      <h1
+        style={{
+          position: "absolute",
+          width: "100%",
+          margin: "auto",
+          textAlign: "center"
+        }}
+      >
+        {dimensions.width + " x " + dimensions.height}
+      </h1>
     </header>
   );
 };
