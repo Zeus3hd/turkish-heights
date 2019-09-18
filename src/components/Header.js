d0556a000296372398f844a0db74d15d77a53bdf
unknown <dafuq.ahd@gmail.com>
2019-09-18 18:03:33 +0300

    add ::after for the header to add header content

--- src/components/Header.js
+++ src/components/Header.js
@@ -42,39 +42,11 @@ const Header = () => {
             zIndex: -1
           }}
         >
-          <source
-            src="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"
-            type="video/mp4"
-          ></source>
+          <source src={headerVideo} type="video/mp4"></source>
         </video>
       ) : (
         <div id="mobile-header"></div>
       )}
-      {/*<video
-        autoPlay
-        loop
-        muted
-        style={{
-          width: "100%",
-          height: "100vh",
-          margin: 0,
-          objectFit: "fill",
-          position: "absolute",
-          zIndex: -1
-        }}
-      >
-        <source src={headerVideo} type="video/mp4"></source>
-      </video>*/}
-      {/*<h1
-        style={{
-          position: "absolute",
-          width: "100%",
-          margin: "auto",
-          textAlign: "center"
-        }}
-      >
-        {dimensions.width + " x " + dimensions.height}
-      </h1>*/}
     </header>
   );
 };
