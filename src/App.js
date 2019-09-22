4da20d786a82fe53faf828d671892f1397764527
unknown <dafuq.ahd@gmail.com>
2019-09-22 20:00:53 +0300

    add features section component and style it

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import "./App.css";
 import Navbar from "./components/Navbar";
 import Gallery from "./components/Gallery";
 import Header from "./components/Header";
+import Features from "./components/Features";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Header />
+      <Features />
       <Gallery />
     </div>
   );
